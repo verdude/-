102f39b338ff61be564bda8fc82c6d2ad6a30bf7
Santiago Verdu <erra@qmake.org>
2024-09-17 10:41:35 -0600

    i

--- linzhong.py
+++ linzhong.py
@@ -1,6 +1,7 @@
 import argparse
 import ctypes
 import logging
+import platform
 
 logging.basicConfig(level=logging.INFO, handlers=[logging.StreamHandler()])
 for handler in logging.getLogger().handlers:
@@ -9,7 +10,7 @@ for handler in logging.getLogger().handlers:
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Get a TOTP code")
+    parser = argparse.ArgumentParser(description="獲取一個TOTP碼")
     parser.add_argument("-f", "--fuwu", type=str, help="服務命名")
     return parser.parse_args()
 
@@ -31,14 +32,21 @@ def main():
         logging.error("服務命名不能為空")
         exit(1)
 
-    lib = set_argtypes("./zig-out/lib/lib臨終.dylib")
+    system = platform.system()
+    if system == "Linux":
+        lib = set_argtypes("./zig-out/lib/lib臨終.so")
+    elif system == "Darwin":
+        lib = set_argtypes("./zig-out/lib/lib臨終.dylib")
+    else:
+        logging.error("不支持的操作系統")
+        exit(1)
 
     fuwu_bytes = args.fuwu.encode("utf-8")
     fuwu_len = len(fuwu_bytes)
 
     totp_code = lib.get_totp(fuwu_bytes, fuwu_len)
 
-    print(totp_code)
+    print(str(totp_code))
 
 
 if __name__ == "__main__":
