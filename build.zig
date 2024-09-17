ea39b7c125db8b28c9a7dd879243f4b15570ab64
Santiago Verdu <erra@qmake.org>
2024-09-16 20:50:18 -0600

    shijianmima

--- build.zig
+++ build.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-const sdl = @import("sdl");
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
@@ -16,23 +15,12 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    // Initialize the SDL2 SDK
-    const sdk = sdl.init(b, .{});
-
     const exe = b.addExecutable(.{
         .name = "臨終",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    // Link SDL2 as a shared library
-    sdk.link(exe, .dynamic, sdl.Library.SDL2);
-
-    // Add "sdl2" package that exposes the SDL2 API
-    exe.root_module.addImport("sdl2", sdk.getNativeModule());
-
-    exe.linkSystemLibrary("SDL2");
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
--- src/main.zig
+++ src/main.zig
@@ -1,34 +1,71 @@
 const std = @import("std");
-const crypto = @import("std.crypto");
-const base32 = @import("std.base32");
+const hmac = std.crypto.auth.hmac.HmacSha1;
 
-fn readStringFromStdin() ![]u8 {
-    const stdin = std.io.getStdIn().reader();
-    var buffer: [256]u8 = undefined;
-    const read_bytes = try stdin.readUntilDelimiterOrEof(&buffer, '\n');
-    return buffer[0..read_bytes];
+fn base32Decode(input: []const u8) ![]u8 {
+    var output: [256]u8 = undefined;
+    var output_index: usize = 0;
+    var buffer: u32 = 0;
+    var bits_left: u5 = 0;
+
+    for (input) |c| {
+        std.debug.print("c: 0x{x}, 【{c}】\n", .{ c, c });
+        const value: u8 = switch (c) {
+            'A'...'Z' => c - 'A',
+            '2'...'7' => c - '2' + 26,
+            else => {
+                std.debug.print("Invalid character: 0x{x}, 【{c}】\n", .{ c, c });
+                return error.InvalidCharacter;
+            },
+        };
+
+        buffer = (buffer << 5) | (value & 0x1F);
+        bits_left += 5;
+
+        while (bits_left >= 8) {
+            bits_left -= 8;
+            std.debug.print("buffer: 0x{x}, bits_left: {d}\n", .{ buffer, bits_left });
+            output[output_index] = @intCast((buffer >> bits_left) & 0xFF);
+            output_index += 1;
+        }
+    }
+
+    return output[0..output_index];
 }
 
-fn generateTotp(secret: []const u8, time_step: u64, digits: u8) ![]u8 {
-    const time = std.time.milliTimestamp() / (time_step * 1000);
+fn readStringFromStdin(buffer: []u8) ![]u8 {
+    const stdin = std.io.getStdIn().reader();
+    const read_bytes = try stdin.readUntilDelimiterOrEof(buffer, '\n');
+    return buffer[0..read_bytes.?.len];
+}
+
+fn generateTotp(secret: []const u8, time_step: i64, digits: u8, nali: []u8) ![]u8 {
+    const time = @divTrunc(std.time.milliTimestamp(), time_step * 1000);
     var time_bytes: [8]u8 = undefined;
-    std.mem.writeInt(u64, &time_bytes, time);
+    std.mem.writeInt(u64, &time_bytes, @intCast(time), .little);
 
-    var hmac = try crypto.hmac(crypto.hash.sha1, secret, &time_bytes);
-    const offset = hmac[hmac.len - 1] & 0x0F;
-    const code = (std.mem.readInt(u32, hmac[offset .. offset + 4]) & 0x7FFFFFFF) % std.math.pow(10, digits);
+    var hmac_result: [hmac.mac_length]u8 = undefined;
+    hmac.create(&hmac_result, &time_bytes, secret);
 
-    var result: [6]u8 = undefined;
-    try std.fmt.bufPrint(&result, "{d:0>6}", .{code});
-    return result[0..digits];
+    const last_byte = hmac_result[hmac_result.len - 1];
+    const offset = last_byte & 0x0F;
+
+    const hmac_slice = hmac_result[offset .. offset + 4];
+    const truncated_hash = std.mem.readInt(u32, @ptrCast(hmac_slice.ptr), .big) & 0x7FFFFFFF;
+
+    const power = std.math.pow(u32, 10, digits);
+    const code = truncated_hash % power;
+
+    return try std.fmt.bufPrint(nali, "{d:0>6}", .{code});
 }
 
 pub fn main() !void {
     std.debug.print("請輸入編碼的密碼：", .{});
-    const secret = try readStringFromStdin();
-    const decoded_secret = try base32.decodeAlloc(std.heap.page_allocator, secret);
-    defer std.heap.page_allocator.free(decoded_secret);
+    var buffer: [256]u8 = undefined;
+    const secret = try readStringFromStdin(&buffer);
+    const decoded_secret = try base32Decode(secret);
+    std.log.debug("Decoded secret: {x}\n", .{decoded_secret});
+    var totp_code: [6]u8 = undefined;
 
-    const totp_code = try generateTotp(decoded_secret, 30, 6);
-    std.debug.print("時間密碼: {s}\n", .{totp_code});
+    const shijianmima = try generateTotp(decoded_secret, 30, 6, &totp_code);
+    std.debug.print("時間密碼: {s}\n", .{shijianmima});
 }
